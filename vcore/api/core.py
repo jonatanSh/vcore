efc830615694ea4674acf0facb6c761fa21aadd5
Jonathan <jonatanshimon@gmail.com>
2018-08-30 22:06:09 +0300

    add support to many plugins in one file

--- vcore/api/core.py
+++ vcore/api/core.py
@@ -15,29 +15,31 @@ def setup(app):
 
     for service_name, service_settings in Settings.plugins.items():
         base_uri = "/" + service_name + "/{0}"
-        for plugin_path, plugin_settings in service_settings.PLUGINS.items():
-            sys_path = os.path.join(Settings.settings.BASE_DIR, os.path.dirname(plugin_path))
-            import_name = os.path.basename(plugin_path).replace(".py", "")
-            sys.path.append(sys_path)
+        for plugin_path in service_settings.PLUGINS:
+            for plugin in service_settings.PLUGINS[plugin_path]:
+                sys_path = os.path.join(Settings.settings.BASE_DIR, os.path.dirname(plugin_path))
+                import_name = os.path.basename(plugin_path).replace(".py", "")
+                sys.path.append(sys_path)
 
-            try:
-                module = __import__(import_name)
-            except Exception as error:
-                logging.error("Plugin import error: {0} in {1}, import_name = {2}".format(error, sys_path, import_name))
-                sys.path.remove(sys_path)
-                continue
+                try:
+                    module = __import__(import_name)
+                except Exception as error:
+                    logging.error(
+                        "Plugin import error: {0} in {1}, import_name = {2}".format(error, sys_path, import_name))
+                    sys.path.remove(sys_path)
+                    continue
 
-            try:
-                handler = getattr(module, plugin_settings.handler)
-            except Exception as error:
-                logging.error("Plugin handler not found error: {0}".format(error))
-                sys.path.remove(sys_path)
-                continue
+                try:
+                    handler = getattr(module, plugin["handler"])
+                except Exception as error:
+                    logging.error("Plugin handler not found error: {0}".format(error))
+                    sys.path.remove(sys_path)
+                    continue
 
-            for route in plugin_settings.routes:
-                current_route = base_uri.format(route)
-                Settings.runtime.routes.append(current_route)
-                Settings.runtime.api.add_resource(handler, current_route)
+                for route in plugin["routes"]:
+                    current_route = base_uri.format(route)
+                    Settings.runtime.routes.append(current_route)
+                    Settings.runtime.api.add_resource(handler, current_route)
 
 
 def run(app):
