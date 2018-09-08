83e3fbcbc1f0412cde8b7a8586b6a553031bbefe
Jonathan <jonatanshimon@gmail.com>
2018-09-09 02:01:48 +0300

    add arguments

--- vcore/api/plugins/docker/builder.py
+++ vcore/api/plugins/docker/builder.py
@@ -1,3 +1,4 @@
+from flask_restful import reqparse
 from vcore.tasks.api.task_objects import Task
 from vcore.services.docker.builder.builder import build_image
 from vcore.api.plugins.utils import async_request
@@ -5,4 +6,8 @@ from vcore.api.plugins.utils import async_request
 
 class Builder(Task):
     def task_handle_post(self, *args, **kwargs):
-        return async_request(build_image.apply_async(args, kwargs, queue="task_builder"))
+        parse = reqparse.RequestParser()
+        parse.add_argument('file_id')
+        parse.add_argument("target")
+        options = parse.parse_args()
+        return async_request(build_image.apply_async(args, options, queue="task_builder"))
